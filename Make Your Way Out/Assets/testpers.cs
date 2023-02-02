28cb7b8a145bcac6400ef79488fe81a138654287
Tiger1413 <arabsplayer@hotmail.com>
2023-02-02 21:25:29 +0300

    tad bit better but i gave up lol

--- Make Your Way Out/Assets/testpers.cs
+++ Make Your Way Out/Assets/testpers.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class testpers : MonoBehaviour
 {
+    float maxDistance = 0;
+
     GrabScript grab;
     Camera cam;
     Rigidbody rb;
+
+    [Header("Visual Veriables")]
+    [SerializeField] List<MeshRenderer> renderers;
+    [Space(10)]
 
     [Header("Lerp Variables")]
     [SerializeField] float overTime = 0.5f;
@@ -14,7 +20,7 @@ public class testpers : MonoBehaviour
 
     [Header("Camera Variables")]
     [SerializeField] float orthoSize;
-    [Space (10)]
+    [Space(10)]
 
     [Header("Rotation Variables")]
     [SerializeField] Vector3 goalRotMin;
@@ -32,17 +38,18 @@ public class testpers : MonoBehaviour
     {
         localRot = transform.rotation.eulerAngles;
 
-        if(grab.IsGrabbed == true)
+        if (grab.IsGrabbed == true)
         {
             switchtoOrtho();
+            //calculateDistance();
         }
         else
         {
             cam.orthographic = false;
         }
 
-        if (transform.eulerAngles.x > goalRotMin.x && transform.eulerAngles.y > goalRotMin.y
-        &&  transform.eulerAngles.x < goalRotMax.x && transform.eulerAngles.y < goalRotMax.y)
+        if (transform.eulerAngles.x > goalRotMin.x && transform.eulerAngles.y > goalRotMin.y && transform.eulerAngles.z > goalRotMin.z
+        && transform.eulerAngles.x < goalRotMax.x && transform.eulerAngles.y < goalRotMax.y && transform.eulerAngles.z < goalRotMax.z)
         {
             StartCoroutine(lerpToGoal());
             grab.Drop();
@@ -61,11 +68,28 @@ public class testpers : MonoBehaviour
     IEnumerator lerpToGoal()
     {
         float startTime = Time.time;
-        while(Time.time < startTime + overTime)
+        while (Time.time < startTime + overTime)
         {
-            transform.rotation = Quaternion.Lerp(Quaternion.Euler(transform.eulerAngles) ,Quaternion.Euler(goalRot), (Time.time - startTime)/overTime);
+            transform.rotation = Quaternion.Lerp(Quaternion.Euler(transform.eulerAngles), Quaternion.Euler(goalRot), (Time.time - startTime) / overTime);
             yield return null;
         }
         transform.eulerAngles = goalRot;
+    }
+
+    void calculateDistance()
+    {
+        float distance = Vector3.Distance(transform.eulerAngles, goalRot);
+
+        foreach (MeshRenderer mr in renderers)
+        {
+            mr.material.color = Color.Lerp(Color.green, Color.red, Mathf.InverseLerp(0, 507, distance));
+        }
+        print(Mathf.InverseLerp(0, 507, distance));
+        //if(distance > maxDistance)
+        //{
+        //    maxDistance = distance;
+        //    print("New Max = " + maxDistance);
+        //}
+
     }
 }
