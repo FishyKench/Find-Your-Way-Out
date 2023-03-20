2a3c3d3b744b2805afe2d66ea3637336c99f1045
FishyKench <m7md.al7male@gmail.com>
2023-03-20 23:48:10 +0300

    fully done with no SFX (no scum)

---
+++ Make Your Way Out/Assets/ScumInteract.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScumInteract : interactable
+{
+    void Start()
+    {
+        _isOff = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void OnFocus()
+    {
+
+    }
+
+    public override void OnInteract()
+    {
+        StartCoroutine(rotateLever());
+    }
+
+    public override void OnLoseFocus()
+    {
+
+    }
+}
