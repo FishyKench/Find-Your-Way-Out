ad64f2f81b210c6295c94620702b487261b58489
Tiger1413 <arabsplayer@hotmail.com>
2023-03-22 01:30:49 +0300

    final demo ??

--- Make Your Way Out/Assets/ScumInteract.cs
+++ Make Your Way Out/Assets/ScumInteract.cs
@@ -6,10 +6,11 @@ public class ScumInteract : interactable
 {
 
     public GameObject endScreenImg;
+    private Animator fade;
     void Start()
     {
         //_isOff = true;
-
+        fade = GameObject.Find("Fade").GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -36,7 +37,10 @@ public class ScumInteract : interactable
     IEnumerator EndScreen()
     {
         endScreenImg.SetActive(true);
-        yield return new WaitForSeconds(4f);
+        yield return new WaitForSeconds(3f);
+        fade.SetTrigger("FadeIn");
+        yield return new WaitForSeconds(1f);
+        print("QUIT");
         Application.Quit(0);
     }
 }
