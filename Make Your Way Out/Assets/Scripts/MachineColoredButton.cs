de86c5a174870d2b857e116b0b385ef715e7392a
FishyKench <m7md.al7male@gmail.com>
2023-04-10 21:11:51 +0300

    Machine working?

---
+++ Make Your Way Out/Assets/Scripts/MachineColoredButton.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MachineColoredButton : interactable
+{
+    [SerializeField]
+    private int _buttonID;
+
+    public MachineManager machineM;
+    public PasswordInteract passInter;
+
+
+    public GameObject RedBlob, BlueBlob,GreenBlob;
+
+    public TMP_Text textIndicator;
+
+
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void OnFocus()
+    {
+
+    }
+
+    public override void OnInteract()
+    {
+        
+        if(machineM.solutionString.Length < 7 && passInter.machineNotRunning == true)
+        { 
+            switch (_buttonID)
+            {
+                case 1:
+                    passInter.lilScreenText.text = "";
+                    machineM.solutionString = machineM.solutionString + "R"; //A
+                    StartCoroutine(DisplayColors());
+                    textIndicator.text += "R";
+                    break;
+                case 2:
+                    passInter.lilScreenText.text = "";
+                    machineM.solutionString = machineM.solutionString + "B"; //B
+                    StartCoroutine(DisplayColors());
+                    textIndicator.text += "B";
+                    break;
+                case 3:
+                    passInter.lilScreenText.text = "";
+                    machineM.solutionString = machineM.solutionString + "G"; //C
+                    StartCoroutine(DisplayColors());
+                    textIndicator.text += "G";
+                    break;
+            }
+        }
+    }
+
+    public override void OnLoseFocus()
+    {
+
+    }
+
+    IEnumerator DisplayColors()
+    {
+        switch (_buttonID)
+        {
+            case 1:
+                RedBlob.SetActive(true);
+                yield return new WaitForSeconds(.5f);
+                RedBlob.SetActive(false);
+                break;
+            case 2:
+                BlueBlob.SetActive(true);
+                yield return new WaitForSeconds(.5f);
+                BlueBlob.SetActive(false);
+                break;
+            case 3:
+                GreenBlob.SetActive(true);
+                yield return new WaitForSeconds(.5f);
+                GreenBlob.SetActive(false);
+                break;
+        }
+    }
+
+
+}
---
+++ Make Your Way Out/Assets/Scripts/PasswordInteract.cs
@@ -0,0 +1,171 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PasswordInteract : interactable
+{
+    [SerializeField]
+    private int buttonNo;
+
+    public TMP_Text lilScreenText;
+
+    public bool textIsCorrect;
+
+    public GameObject firstImg;
+    public GameObject secondImg;
+    public GameObject thridImg;
+
+
+    public bool machineNotRunning = true; // checks if slide show is running or not
+
+
+    public MachineManager machineM;
+
+    public MachineColoredButton coloredButtons;
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(lilScreenText.text == "Z32X")
+        {
+            textIsCorrect = true;
+        }
+
+        if(lilScreenText.text.Length == 4)
+        {
+            if(textIsCorrect == true)
+            {
+                StartCoroutine(SlideShow());
+                lilScreenText.text = "";
+            }
+            else
+            {
+                lilScreenText.text = "";
+            }
+        }
+
+
+    }
+
+    public override void OnFocus()
+    {
+
+    }
+
+    public override void OnInteract()
+    {
+
+
+        switch (buttonNo)
+        {
+            case 1:
+                if (lilScreenText.text.Length <= 4)
+                {
+                    coloredButtons.textIndicator.text = "";
+                    lilScreenText.text = lilScreenText.text + "X";
+                }
+                    
+                break;
+            case 2:
+                if (lilScreenText.text.Length <= 4)
+                {
+                    coloredButtons.textIndicator.text = "";
+                    lilScreenText.text = lilScreenText.text + "Y";
+                }
+                break;
+            case 3:
+                if (lilScreenText.text.Length <= 4)
+                {
+                    coloredButtons.textIndicator.text = "";
+                    lilScreenText.text = lilScreenText.text + "Z";
+                }
+                break;
+            case 4:
+                if (lilScreenText.text.Length <= 4)
+                {
+                    coloredButtons.textIndicator.text = "";
+                    lilScreenText.text = lilScreenText.text + "1";
+                }
+                break;
+            case 5:
+                if (lilScreenText.text.Length <= 4)
+                {
+                    coloredButtons.textIndicator.text = "";
+                    lilScreenText.text = lilScreenText.text + "2";
+                }
+                break;
+            case 6:
+                if (lilScreenText.text.Length <= 4)
+                {
+                    coloredButtons.textIndicator.text = "";
+                    lilScreenText.text = lilScreenText.text + "3";
+                }
+                break;
+        }
+
+
+
+    }
+
+    public override void OnLoseFocus()
+    {
+
+    }
+
+
+    IEnumerator SlideShow()
+    {
+        machineNotRunning = false;
+        machineM.solutionString = "";
+        textIsCorrect = false; // making the text false again
+
+        firstImg.SetActive(true);
+        yield return new WaitForSeconds(1f);  // 1st img is displayed.. 
+        firstImg.SetActive(false);
+        secondImg.SetActive(true);
+
+        yield return new WaitForSeconds(1f); // 2nd img is displayed.. 
+        secondImg.SetActive(false);
+        thridImg.SetActive(true);
+
+        yield return new WaitForSeconds(1f); // 3rd img is displayed... 
+        secondImg.SetActive(false);
+        thridImg.SetActive(true);
+
+
+        yield return new WaitForSeconds(1f); // 2nd img is displayed... (again)
+        thridImg.SetActive(false); 
+        secondImg.SetActive(true);
+
+        yield return new WaitForSeconds(1f); // 1st img is displayed... (again)
+        secondImg.SetActive(false);
+        firstImg.SetActive(true);
+
+
+        yield return new WaitForSeconds(1f); // 3rd img is displayed... (again)
+        firstImg.SetActive(false);
+        thridImg.SetActive(true);
+
+
+        yield return new WaitForSeconds(1f); // 1st img is displayed... (again)
+        thridImg.SetActive(false);
+        firstImg.SetActive(true);
+
+
+        yield return new WaitForSeconds(1f);  //screen is off
+        firstImg.SetActive(false);
+
+        yield return new WaitForSeconds(0.5f);
+        machineNotRunning = true;
+
+    }
+
+}
