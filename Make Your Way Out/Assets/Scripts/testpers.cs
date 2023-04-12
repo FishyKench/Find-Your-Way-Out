cd5f945aae81d7988f5144bcb1e66655e507247b
Tiger1413 <arabsplayer@hotmail.com>
2023-04-12 22:16:25 +0300

    awewewewewewe

--- Make Your Way Out/Assets/Scripts/testpers.cs
+++ Make Your Way Out/Assets/Scripts/testpers.cs
@@ -4,29 +4,30 @@ using UnityEngine;
 
 public class testpers : MonoBehaviour
 {
-    float maxDistance = 0;
+    //float maxDistance = 0;
 
     GrabScript grab;
     Camera cam;
     Rigidbody rb;
 
-    [Header("Visual Veriables")]
-    [SerializeField] List<MeshRenderer> renderers;
-    [Space(10)]
+    //[Header("Visual Veriables")]
+    //[SerializeField] List<MeshRenderer> renderers;
+    //[Space(10)]
 
-    [Header("Lerp Variables")]
-    [SerializeField] float overTime = 0.5f;
-    [Space(10)]
+    //[Header("Lerp Variables")]
+    //[SerializeField] float overTime = 0.5f;
+    //[Space(10)]
 
-    [Header("Camera Variables")]
-    [SerializeField] float orthoSize;
-    [Space(10)]
+    //[Header("Camera Variables")]
+    //[SerializeField] float orthoSize;
+    //[Space(10)]
 
-    [Header("Rotation Variables")]
-    [SerializeField] Vector3 goalRotMin;
-    [SerializeField] Vector3 goalRotMax;
-    [SerializeField] Vector3 goalRot;
+    //[Header("Rotation Variables")]
+    //[SerializeField] Vector3 goalRotMin;
+    //[SerializeField] Vector3 goalRotMax;
+    //[SerializeField] Vector3 goalRot;
     [SerializeField] Vector3 localRot;
+    [SerializeField]bool canMove = true;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -40,56 +41,61 @@ public class testpers : MonoBehaviour
 
         if (grab.IsGrabbed == true)
         {
-            switchtoOrtho();
+            canMove = false;
+            cam.GetComponent<PlayerCam>().enabled = false;
+            FindObjectOfType<PlayerMovementAdvanced>().enabled = false;
+            //switchtoOrtho();
             //calculateDistance();
         }
-        else
+        else if(grab.IsGrabbed == false && canMove == false)
         {
-            cam.orthographic = false;
+            canMove = true;
+            FindObjectOfType<PlayerMovementAdvanced>().enabled = true;
+            cam.GetComponent<PlayerCam>().enabled = true;
         }
 
-        if (transform.eulerAngles.x > goalRotMin.x && transform.eulerAngles.y > goalRotMin.y && transform.eulerAngles.z > goalRotMin.z
-        && transform.eulerAngles.x < goalRotMax.x && transform.eulerAngles.y < goalRotMax.y && transform.eulerAngles.z < goalRotMax.z)
-        {
-            StartCoroutine(lerpToGoal());
-            grab.Drop();
-            rb.isKinematic = true;
-            rb.freezeRotation = true;
-        }
-    }
-
-    void switchtoOrtho()
-    {
-        cam.orthographic = true;
-        cam.orthographicSize = orthoSize;
-        cam.GetComponent<PlayerCam>().enabled = false;
-    }
-
-    IEnumerator lerpToGoal()
-    {
-        float startTime = Time.time;
-        while (Time.time < startTime + overTime)
-        {
-            transform.rotation = Quaternion.Lerp(Quaternion.Euler(transform.eulerAngles), Quaternion.Euler(goalRot), (Time.time - startTime) / overTime);
-            yield return null;
-        }
-        transform.eulerAngles = goalRot;
-    }
-
-    void calculateDistance()
-    {
-        float distance = Vector3.Distance(transform.eulerAngles, goalRot);
-
-        foreach (MeshRenderer mr in renderers)
-        {
-            mr.material.color = Color.Lerp(Color.green, Color.red, Mathf.InverseLerp(0, 507, distance));
-        }
-        print(Mathf.InverseLerp(0, 507, distance));
-        //if(distance > maxDistance)
+        //if (transform.eulerAngles.x > goalRotMin.x && transform.eulerAngles.y > goalRotMin.y && transform.eulerAngles.z > goalRotMin.z
+        //&& transform.eulerAngles.x < goalRotMax.x && transform.eulerAngles.y < goalRotMax.y && transform.eulerAngles.z < goalRotMax.z)
         //{
-        //    maxDistance = distance;
-        //    print("New Max = " + maxDistance);
+        //    StartCoroutine(lerpToGoal());
+        //    grab.Drop();
+        //    rb.isKinematic = true;
+        //    rb.freezeRotation = true;
         //}
-
     }
+
+    //void switchtoOrtho()
+    //{
+    //    cam.orthographic = true;
+    //    cam.orthographicSize = orthoSize;
+    //    cam.GetComponent<PlayerCam>().enabled = false;
+    //}
+
+    //IEnumerator lerpToGoal()
+    //{
+    //    float startTime = Time.time;
+    //    while (Time.time < startTime + overTime)
+    //    {
+    //        transform.rotation = Quaternion.Lerp(Quaternion.Euler(transform.eulerAngles), Quaternion.Euler(goalRot), (Time.time - startTime) / overTime);
+    //        yield return null;
+    //    }
+    //    transform.eulerAngles = goalRot;
+    //}
+
+    //void calculateDistance()
+    //{
+    //    float distance = Vector3.Distance(transform.eulerAngles, goalRot);
+
+    //    foreach (MeshRenderer mr in renderers)
+    //    {
+    //        mr.material.color = Color.Lerp(Color.green, Color.red, Mathf.InverseLerp(0, 507, distance));
+    //    }
+    //    print(Mathf.InverseLerp(0, 507, distance));
+    //    //if(distance > maxDistance)
+    //    //{
+    //    //    maxDistance = distance;
+    //    //    print("New Max = " + maxDistance);
+    //    //}
+
+    //}
 }
