71919ed88892051bc5ed792707e71dc9bca709b0
FishyKench <m7md.al7male@gmail.com>
2023-04-13 03:31:27 +0300

    workie machine with animation

--- Make Your Way Out/Assets/ChemMixBtnManager.cs
+++ Make Your Way Out/Assets/ChemMixBtnManager.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ChemMixBtnManager : MonoBehaviour
 {
     // Start is called before the first frame update
 
+    public TMP_Text firstText;
+    public TMP_Text secondText;
+    public TMP_Text thirdText;
+    public TMP_Text fourthText;
 
     public int btnPos;
 
@@ -15,9 +20,13 @@ public class ChemMixBtnManager : MonoBehaviour
 
     }
 
-    // Update is called once per frame
+    // Update is called once per frame //Au Pt Si Fe
     void Update()
     {
+        if(firstText.text == "Au" && secondText.text == "Pt" && thirdText.text == "Si" && fourthText.text == "Fe")
+        {
+            print("momo");
+        }
         
     }
 }
