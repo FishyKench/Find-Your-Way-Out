019f5d173e38d2147e376e77ae2eb023b5fbd874
Tiger1413 <arabsplayer@hotmail.com>
2023-04-15 23:41:58 +0300

    brrrrrra

---
+++ Make Your Way Out/Assets/Scripts/lvl5/DoorLocked.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorLocked : interactable
+{
+    public bool isLocked = true;
+
+    [SerializeField] private Material highlightMat;
+    [SerializeField] private AudioSource slam;
+    [SerializeField] private AudioSource open;
+
+    private Material _defaultMat;
+    private MeshRenderer _meshRenderer;
+
+    [Header("Rotation stuff")]
+    Quaternion closedRotation;
+    private Transform pivot;
+    [SerializeField] private bool invertRotation = false;
+
+    [SerializeField]
+    private Quaternion _targetRot;
+
+    private float elapsedTime;
+    private float waitTime = 0.75f;
+    private Quaternion originalRotation;
+
+    private void Start()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _defaultMat = _meshRenderer.material;
+
+
+        originalRotation = this.transform.rotation;
+
+
+
+
+    }
+    public override void OnFocus()
+    {
+        _meshRenderer.material = highlightMat;
+    }
+
+    public override void OnInteract()
+    {
+        if(isLocked == false)
+            StartCoroutine(openDoor());
+    }
+
+    public override void OnLoseFocus()
+    {
+        _meshRenderer.material = _defaultMat;
+    }
+
+    private IEnumerator openDoor()
+    {
+
+
+        open.PlayOneShot(open.clip);
+
+        while (elapsedTime < waitTime)
+        {
+            transform.rotation = Quaternion.Lerp(originalRotation, _targetRot, (elapsedTime / waitTime));
+            elapsedTime += Time.deltaTime;
+
+            // Yield here
+            yield return null;
+
+
+        }
+        // Make sure we got there
+        transform.rotation = _targetRot;
+        elapsedTime = 0;
+        yield return new WaitForSeconds(2f);
+        slam.PlayOneShot(slam.clip);
+
+        while (elapsedTime < waitTime)
+        {
+            transform.rotation = Quaternion.Lerp(this.transform.rotation, originalRotation, (elapsedTime / waitTime));
+            elapsedTime += Time.deltaTime;
+
+            // Yield here
+            yield return null;
+
+
+        }
+        // Make sure we got there
+        transform.rotation = originalRotation;
+        elapsedTime = 0;
+    }
+}
